3a6035d63d0fdbb06eb3af7f1e5332f9d8ebcdb9
alter <gambarini.simone@studenti.salesiansesto.it>
2023-06-28 14:56:19 +0200

    fit Seat

--- src/main/java/SmartProject/demo/Repository/SeatRepository.java
+++ src/main/java/SmartProject/demo/Repository/SeatRepository.java
@@ -1,7 +1,22 @@
 package SmartProject.demo.Repository;
 
 import SmartProject.demo.Entity.Seat;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SeatRepository extends CrudRepository<Seat, Long> {
+
+    @Query(value = "UPDATE smart_project.seats " +
+            "SET position_left = :position_left, " +
+            "position_top = :position_top, " +
+            "rotation = :rotation, " +
+            "label = :lable " +
+            "WHERE id = :id", nativeQuery = true)
+
+    @Modifying
+    @Transactional
+    void updateSeat(Long id, String lable, String rotation, String position_top, String position_left);
+
 }
