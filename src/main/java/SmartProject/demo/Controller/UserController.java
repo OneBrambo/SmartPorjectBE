6238a4c31d0c2c9a6ceb95fd73f6c848ffc4beb7
alter <gambarini.simone@studenti.salesiansesto.it>
2023-06-28 11:09:36 +0200

    fit User

--- src/main/java/SmartProject/demo/Controller/UserController.java
+++ src/main/java/SmartProject/demo/Controller/UserController.java
@@ -1,4 +1,19 @@
 package SmartProject.demo.Controller;
 
+import SmartProject.demo.Entity.User;
+import SmartProject.demo.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "*")
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PutMapping(value = "/api/modifyUser", consumes = {"application/json"})
+    public void UpdateUser(@RequestBody User userUpdate) {
+        userRepository.updateUser(userUpdate.getId(), userUpdate.getFirstName(), userUpdate.getLastName(), userUpdate.getUsername(), userUpdate.getPhoneNumber(), userUpdate.getSite());
+    }
 }
